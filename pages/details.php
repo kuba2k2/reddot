f7a4466cc1441f0e247b7ee40af0a3c0b7ceb0a7
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-03-31 14:17:09 +0200

    Update profile page

--- pages/details.php
+++ pages/details.php
@@ -1,42 +1,180 @@
 <?php
 db_connect();
-	$stmt = $db->prepare('SELECT * FROM `users` WHERE user_id = ?;');
-	$stmt->execute([
-		$_SESSION['user_id'],
-	]);
-	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt = $db->prepare('SELECT * FROM `users` WHERE user_id = ?;');
+$stmt->execute([
+	isset($_GET['user-id']) ? $_GET['user-id'] : $_SESSION['user_id'],
+]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if (empty($row['pfp'])) {
+	$row['pfp'] = 'user.png';
+}
+
+$dog = 'brak psa :(  (ale ma kota) :)';
+switch (intval($row['dog'])) {
+	case 1:
+		$dog = 'Czarny';
+		break;
+	case 2:
+		$dog = 'Brązowy';
+		break;
+	case 3:
+		$dog = 'Biały';
+		break;
+	case 4:
+		$dog = 'Moręgowaty';
+		break;
+	case 5:
+		$dog = 'Szary';
+		break;
+	case 6:
+		$dog = 'Biało-szary';
+		break;
+	case 7:
+		$dog = 'Czarno-biały';
+		break;
+	case 8:
+		$dog = 'Brązowo-biały';
+		break;
+	case 9:
+		$dog = 'Czarno-brązowy';
+		break;
+	case 10:
+		$dog = 'Czarno-biało-brązowy';
+		break;
+	case 11:
+		$dog = 'Złoty';
+		break;
+	case 12:
+		$dog = 'Zielony';
+		break;
+}
+
+$edit = isset($_GET['edit']);
+if ($edit && !can_edit($row['user_id'])) {
+	header('Location: index.php');
+	exit;
+}
+
+$disabled = 'disabled';
+$value = 'placeholder';
+if ($edit) {
+	$disabled = '';
+	$value = 'value';
+}
+
 ?>
 <div class="container rounded bg-white mt-5 mb-5">
-    <div class="row">
-        <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-        </div>
-        <div class="col-md-5 border-right">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                    <h4 class="text-right">Profile Settings</h4>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="<?= $row["name"] ?>" disabled value=""></div>
-                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" disabled placeholder="surname"></div>
-                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" disabled placeholder="enter address line 1" value=""></div>
-                    <div class="col-md-12"><label class="labels">email</label><input type="text" class="form-control" placeholder="email" disabled value=""></div>
-                    <div class="col-md-12"><label class="labels">login</label><input type="text" class="form-control" placeholder="login"  disabled value=""></div>
-                    <div class="col-md-12"><label class="labels">Date Created</label><input type="text" class="form-control" placeholder="createdata"  disabled value=""></div>
-                    <div class="col-md-12"><label class="labels">logindate</label><input type="text" class="form-control" placeholder="logindate" disabled value=""></div>
-                    <div class="col-md-6"><label class="labels">Dog</label><input type="text" class="form-control" placeholder="dog"  disabled value=""></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div><br>
-                <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
-                <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
-            </div>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-md-4 border-right">
+			<div class="d-flex flex-column align-items-center text-center p-3 py-5">
+				<div class="profile-pic">\
+					<label class="-label" for="file">
+						<span class="glyphicon glyphicon-camera"></span>
+						<span>Change Image</span>
+					</label>
+					<input id="file" type="file" onchange="loadFile(event)" />
+					<img src="<?= $row["pfp"] ?>" id="output" width="200" />
+					<script>
+						var loadFile = function(event) {
+							var image = document.getElementById("output");
+							image.src = URL.createObjectURL(event.target.files[0]);
+						};
+					</script>
+				</div>
+
+				<span class="font-weight-bold">
+					<?= $row["login"] ?>
+				</span>
+				<span class="text-black-50">
+					<?= $row["email"] ?>
+				</span>
+				<span> </span>
+			</div>
+		</div>
+		<div class="col-md-8 border-right">
+			<div class="p-3 py-5">
+				<div class="d-flex justify-content-between align-items-center mb-3">
+					<h4 class="text-right"><?= ($edit ? 'Edytuj swój profil' : 'Profil użytkownika') ?></h4>
+				</div>
+				<?= ($edit ? '<form method="post">' : '') ?>
+				<div class="row mt-3">
+					<div class="col-md-6">
+						<label class="labels">Imię</label>
+						<input type="text" class="form-control" <?= $value ?>="<?= $row["name"] ?>" <?= $disabled ?>>
+					</div>
+					<div class="col-md-6">
+						<label class="labels">Nazwisko</label>
+						<input type="text" class="form-control" <?= $value ?>="<?= $row["surname"] ?>" <?= $disabled ?>>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Adres</label>
+						<input type="text" class="form-control" <?= $value ?>="<?= $row["address"] ?>" <?= $disabled ?>>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Email</label>
+						<input type="text" class="form-control" <?= $value ?>="<?= $row["email"] ?>" <?= $disabled ?>>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Login</label>
+						<input type="text" class="form-control" <?= $value ?>="<?= $row["login"] ?>" <?= $disabled ?>>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Utoworzono</label>
+						<input type="text" class="form-control" placeholder="<?= $row["createdate"] ?>" disabled>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Ostatnie logowanie</label>
+						<input type="text" class="form-control" placeholder="<?= $row["logindate"] ?>" disabled>
+					</div>
+					<div class="col-md-12">
+						<label class="labels">Pies</label>
+						<input type="text" class="form-control" placeholder="<?= $dog ?>" disabled>
+					</div>
+				</div>
+				<?php if (!$edit) { ?>
+					<a href="?page=posts&user-id=<?= $row['user_id'] ?>" class="btn btn-outline-primary mt-3">Zobacz posty</a>
+				<?php } ?>
+				<?php if ($edit) { ?>
+					<input type="hidden" name="user-edit-id" value="<?= $row['user_id'] ?>">
+					<button type="submit" class="btn btn-outline-success mt-3 float-end" name="form-user-edit">Zapisz</button>
+					<a class="btn btn-outline-danger mt-3 me-3 float-end" href="?page=details&user-id=<?= $row['user_id'] ?>">Anuluj</a>
+				<?php } elseif (can_edit($row['user_id'])) { ?>
+					<a class="btn btn-outline-primary mt-3 float-end" href="?page=details&edit&user-id=<?= $row['user_id'] ?>">Edytuj</a>
+				<?php } ?>
+				<?= ($edit ? '</form>' : '') ?>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-</div>
+<!--
+
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣶⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⣴⡿⠛⠉⠁⠄⠄⠄⠄⠈⢻⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⢸⣿⡅⠄⠄⠄⠄⠄⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣦⠄⠄⠄
+⠄⠄⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄
+⠄⠄⠄⠄⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄
+⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠛⠛⠛⠃⠄⠄
+⠄⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄
+⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄
+⠄⠄⠄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄
+⠄⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠄⠄⠄⠄
+⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠄⠄⠄⠻⣿⣿⣿⣿⣿⣿⣿⡿⠄⠄⠄⠄⠄
+⠄⠄⢸⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄
+⠄⠄⢸⣿⣿⣿⣿⣿⣧⡀⠄⠄⠄⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄
+⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄⠄
+⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠄
+⠄⠄⠄⢿⣿⣿⣿⣿⣿⣿⡟⠄⠄⠄⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄
+⠄⠄⠄⠈⢿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠘⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⢸⣿⣿⣿⣧⠄⠄⠄⠄⠄⠄⠄⢀⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣆⠄⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣄⠄⠄⠄⠄⠄⠄
+⠄⣀⣀⣤⣾⣿⣿⣿⣿⡿⠟⠄⠄⠄⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿⣷⣄⣀⠄⠄⠄
+⠸⠿⠿⠿⠿⠿⠿⠟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠛⠿⢿⡿⠿⠿⠿⠃⠄⠄
+-->
