c7662b71c367030195f91852bcd5aec5bd22e34c
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-03-31 14:55:08 +0200

    Show comments from database

--- pages/post.php
+++ pages/post.php
@@ -12,56 +12,68 @@
 			exit;
 		}
 
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			$stmt2 = $db->prepare('Select * from `pics` where `post_id` = ?;');
-			$stmt2->execute([$row['post_id']]);
-			$pics = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-			if (empty($row['pfp'])) {
-				$row['pfp'] = 'user.png';
-			}
-
-			postCard($row, $pics);
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$stmt2 = $db->prepare('Select * from `pics` where `post_id` = ?;');
+		$stmt2->execute([$row['post_id']]);
+		$pics = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+		if (empty($row['pfp'])) {
+			$row['pfp'] = 'user.png';
 		}
+
+		postCard($row, $pics);
+
 		?>
 	</div>
 	<div class="col-md-12 col-lg-10 col-xl-8">
 		<div class="card">
 			<div class="card-body p-4">
-
 				<div class="col">
-					<div class="d-flex flex-start">
-						<img class="rounded-circle shadow-1-strong me-3" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp" alt="avatar" width="65" height="65" />
-						<div class="flex-grow-1 flex-shrink-1">
-							<div class="d-flex justify-content-between align-items-center">
-								<p class="mb-1">
-									Maria Smantha <span class="small">- 2 hours ago</span>
+					<?php
+					$stmt = $db->prepare('SELECT * FROM comments JOIN users USING(user_id) WHERE post_id = ? ORDER BY date_added DESC;');
+					$stmt->execute([$row['post_id']]);
+
+					$cls = '';
+
+					while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+						if (empty($row['pfp'])) {
+							$row['pfp'] = 'user.png';
+						}
+					?>
+						<div class="d-flex flex-start <?=$cls ?>">
+							<img class="rounded-circle shadow-1-strong me-3" src="<?= $row['pfp'] ?>" alt="avatar" width="65" height="65" />
+							<div class="flex-grow-1 flex-shrink-1">
+								<div class="d-flex justify-content-between align-items-center">
+									<p class="mb-1">
+										<?= $row['login'] ?> <span class="small">- <?= $row['date_added'] ?></span>
+									</p>
+								</div>
+								<p class="small mb-0">
+									<?= nl2br($row['text']) ?>
 								</p>
 							</div>
-							<p class="small mb-0">
-								It is a long established fact that a reader will be distracted by
-								the readable content of a page.
-							</p>
 						</div>
-
-					</div>
-
-					<div class="d-flex flex-start mt-4">
-						<img class="rounded-circle shadow-1-strong me-3" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(12).webp" alt="avatar" width="65" height="65" />
-						<div class="flex-grow-1 flex-shrink-1">
-							<div class="d-flex justify-content-between align-items-center">
-								<p class="mb-1">
-									Natalie Smith <span class="small">- 2 hours ago</span>
-								</p>
-							</div>
-							<p class="small mb-0">
-								The standard chunk of Lorem Ipsum used since the 1500s is
-								reproduced below for those interested. Sections 1.10.32 and
-								1.10.33.
-							</p>
-						</div>
-					</div>
+					<?php
+						$cls = 'mt-4';
+					}
+					?>
 				</div>
 			</div>
 		</div>
+		<div class="card-footer py-3 border-0" style="background-color: #f8f9fa;">
+            <div class="d-flex flex-start w-100">
+              <img class="rounded-circle shadow-1-strong me-3" src="<?= $row['pfp'] ?>" alt="avatar" width="40" height="40" />
+              <div class="form-outline w-100">
+                <textarea
+                  class="form-control"
+                  id="textAreaExample"
+                  rows="4"
+                  style="background: #fff;"
+                ></textarea>
+                <label class="form-label" for="textAreaExample">Message</label>
+              </div>
+            </div>
+            <div class="float-end mt-2 pt-1">
+              <button type="button" class="btn btn-primary btn-sm">Post comment</button>
+              <button type="button" class="btn btn-outline-primary btn-sm">Cancel</button>
 	</div>
 </div>
