7ef4c86873d97d8022a952972bb174d73a4b7ffc
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-03-31 14:14:32 +0200

    Use is_form_complete for registration

--- forms/reg.php
+++ forms/reg.php
@@ -2,25 +2,34 @@
 
 db_connect();
 
-if(isset($_POST["reg-username"]) && isset($_POST["reg-password"]) && isset($_POST["reg-email"]) && isset($_POST["reg-passwordconfirm"])
-&& isset($_POST["reg-name"]) && isset($_POST["reg-surname"]) && isset($_POST["reg-color"]) && isset($_POST["reg-address"])){
-    if($_POST["reg-password"] == $_POST["reg-passwordconfirm"]){
+$fields = [
+    'reg-username',
+    'reg-email',
+    'reg-password',
+    'reg-passwordconfirm',
+    'reg-name',
+    'reg-surname',
+    'reg-color',
+    'reg-address',
+];
+
+if (!is_form_complete($fields)) {
+    if ($_POST['reg-password'] == $_POST['reg-passwordconfirm']) {
         $stmt = $db->prepare("INSERT INTO `users`(`email`, `login`, `password`, `name`, `surname`, `address`, `dog`)
         VALUES (?,?,?,?,?,?,?)"); // ale to fajnewiem
         $stmt->execute([
-            $_POST["reg-email"],
-            $_POST["reg-username"],
-            $_POST["reg-password"],
-            $_POST["reg-name"],
-            $_POST["reg-surname"],
-            $_POST["reg-address"],
-            $_POST["reg-color"],
+            $_POST['reg-email'],
+            $_POST['reg-username'],
+            $_POST['reg-password'],
+            $_POST['reg-name'],
+            $_POST['reg-surname'],
+            $_POST['reg-address'],
+            $_POST['reg-color'],
         ]);
-        add_message("success", 3, "zostałeś zajerestrowany");
+        add_message('success', 3, 'Zostałeś zajerestrowany');
+    } else {
+        add_message('danger', 6, "Hasła nie są takie same");
     }
-    else{
-        add_message("danger", 3, "Hasła nie są takie same");
-    }
+} else {
+    add_message('danger', 7, 'Brakujące pola w formularzu!');
 }
-
-?>
